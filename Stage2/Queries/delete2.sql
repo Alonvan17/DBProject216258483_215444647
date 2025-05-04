f7720eb813c892451cf01b9276fd98b2c964c181
Alonvan17 <154732351+Alonvan17@users.noreply.github.com>
2025-05-04 14:02:09 +0300

    Update delete2.sql

--- Stage2/Queries/delete2.sql
+++ Stage2/Queries/delete2.sql
@@ -1,9 +1,10 @@
---  מוחק כל כלי שיט שתאריך הבדיקה והתאריך שבו מסתיים החוזה הם באותו חודש ושנה והתאריך שבו מסתיים החוזה מאוחר יותר מתאריך הבדיקה
-DELETE FROM Sea_vessel
-WHERE sea_ID IN (
-  SELECT sv.sea_ID
-  FROM Sea_vessel sv
-  WHERE EXTRACT(YEAR FROM sv.lease_expiration_date) = EXTRACT(YEAR FROM sv.test_date)
-    AND EXTRACT(MONTH FROM sv.lease_expiration_date) = EXTRACT(MONTH FROM sv.test_date)
-    AND sv.lease_expiration_date > sv.test_date
-);
+--  השאילתה מוחקת חיילים בדרגת סיילור שלא משמשים כמפקדים וששייכים לצוותים שלהם כלי שיט שנבדק לפני 2015.
+
+DELETE FROM Soldier s
+WHERE s.rank = 'sailor'
+  AND s.s_ID NOT IN (SELECT s_id FROM Commander)
+  AND s.c_ID IN (
+    SELECT DISTINCT sv.c_ID
+    FROM Sea_vessel sv
+    WHERE sv.test_date < DATE '2023-01-01'
+  );
