718a43719eae8a4ffd8e62eeacac132bb81a5d0b
Alonvan17 <154732351+Alonvan17@users.noreply.github.com>
2025-05-04 14:18:29 +0300

    Update update2.sql

--- Stage2/Queries/update2.sql
+++ Stage2/Queries/update2.sql
@@ -1,11 +1,13 @@
---  מעדכן את תאריך סיום ההשאלה של כלי שיט שגודל הצוות שלהם גדול מ־90 והתוקף הנוכחי שלהם במהלך שנת 2023, על ידי הוספת 6 חודשים.
+--  השאילתה מעדכנת את תאריך סיום ההשכרה של כלי שיט שנבדקו לפני 2024 ושייכים לבסיסים שיש בהם לפחות שלושה צוותים שונים, על ידי הוספת חודשיים לתאריך.
+
 UPDATE Sea_vessel sv
-SET lease_expiration_date = lease_expiration_date + INTERVAL '6 months'
-WHERE sea_ID IN (
-    SELECT sv_inner.sea_ID
-    FROM Sea_vessel sv_inner
-    JOIN Crew c ON sv_inner.c_ID = c.c_ID
-    WHERE c.c_size > 90
-      AND sv_inner.lease_expiration_date BETWEEN DATE '2023-01-01' AND DATE '2023-12-31'
-)
-AND lease_expiration_date IS NOT NULL;
+SET lease_expiration_date = lease_expiration_date + INTERVAL '2 months'
+WHERE EXTRACT(YEAR FROM sv.test_date) < 2024
+  AND sv.base_ID IN (
+    SELECT sv2.base_ID
+    FROM Sea_vessel sv2
+    JOIN Crew c ON sv2.c_ID = c.c_ID
+    GROUP BY sv2.base_ID
+    HAVING COUNT(DISTINCT c.c_ID) >= 3
+  )
+  AND lease_expiration_date IS NOT NULL;
