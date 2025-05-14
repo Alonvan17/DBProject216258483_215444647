33aef91a3e0c8b862d46aecd107532bc0baecfd1
Alonvan17 <154732351+Alonvan17@users.noreply.github.com>
2025-05-14 12:31:38 +0300

    Update Commit.sql

--- Stage2/RollbackCommit/Commit.sql
+++ Stage2/RollbackCommit/Commit.sql
@@ -2,10 +2,11 @@ BEGIN;
 
 UPDATE Soldier
 SET name = LEFT('Mr.' || name, 15)
-WHERE rank = 'commander';
+WHERE rank = 'admiral';
 
-SELECT s_id, name, rank FROM Soldier WHERE rank = 'commander';
+SELECT s_id, name, rank FROM Soldier WHERE rank = 'admiral';
 
 COMMIT;
 
-SELECT s_id, name, rank FROM Soldier WHERE rank = 'commander';
+SELECT s_id, name, rank FROM Soldier WHERE rank = 'admiral';
+    
