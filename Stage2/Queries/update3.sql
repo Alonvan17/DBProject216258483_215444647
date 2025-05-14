e20dd067eea01ef6230185bf9b09329d84c437f1
Alonvan17 <154732351+Alonvan17@users.noreply.github.com>
2025-05-14 12:32:09 +0300

    Update update3.sql

--- Stage2/Queries/update3.sql
+++ Stage2/Queries/update3.sql
@@ -1,7 +1,7 @@
 -- השאילתה מעדכנת את שמות החיילים בדרגת אדמירל בצוותים שלהם יש כלי שיט שנבדקו לפני שנת 2023 ובעלי קיבולת מצטברת גבוהה מ-1, על ידי הוספת תחילית לשם
 UPDATE Soldier s
 SET name = 'old_' || s.name
-WHERE rank = 'admiral'
+WHERE rank = 'captain'
 AND c_ID IN (
     SELECT sv.c_ID
     FROM Sea_vessel sv
