99f44712f40907b10ed2c719345ccdf09b3c63fe
Alonvan17 <154732351+Alonvan17@users.noreply.github.com>
2025-06-21 22:27:15 +0300

    Update v1q2.sql

--- Stage3/Views/v1q2.sql
+++ Stage3/Views/v1q2.sql
@@ -2,4 +2,4 @@ SELECT * FROM ARMOR_TANKS_MISSIONS_VIEW;
 
 SELECT tank_id, unit_name, mission_id, mission_date
 FROM ARMOR_TANKS_MISSIONS_VIEW
-WHERE mission_date > '2025-01-01';
+WHERE mission_date > '2023-01-01';
