17cc96e41f68b3e4f7cb811dc95f505cddd6052e
Alonvan17 <154732351+Alonvan17@users.noreply.github.com>
2025-06-24 19:37:30 +0300

    Update triger2.sql

--- Stage4/Trigers/triger2.sql
+++ Stage4/Trigers/triger2.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION trg_sync_delete_soldier_crewmate()
 RETURNS TRIGGER AS $$
 BEGIN
-    DELETE FROM CrewMate WHERE crID = OLD.s_ID;
+    DELETE FROM CrewMate WHERE crID = OLD.sid;
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
